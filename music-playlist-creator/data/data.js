e80447ff4bbb40b45f8bae4a5ea04bbc4392777f
Reena Vollala <reenav@meta.com>
2025-06-04 21:45:00 -0700

    created an array of playlists

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,204 @@
+let playlists=[{
+    playListID:1,
+    playlist_name: "Happy", 
+    playlist_author: "me",
+    playlist_art:"assets/img/playlist.png",
+    songs:[
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+    ]
+},
+{
+    playListID:2,
+    playlist_name: "Happy", 
+    playlist_author: "me",
+    playlist_art:"assets/img/playlist.png",
+    songs:[
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+    ]
+},
+{
+    playListID:3,
+    playlist_name: "Happy", 
+    playlist_author: "me",
+    playlist_art:"assets/img/playlist.png",
+    songs:[
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+    ]
+},
+{
+    playListID:4,
+    playlist_name: "Happy", 
+    playlist_author: "me",
+    playlist_art:"assets/img/playlist.png",
+    songs:[
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+    ]
+},
+{
+    playListID:5,
+    playlist_name: "Happy", 
+    playlist_author: "me",
+    playlist_art:"assets/img/playlist.png",
+    songs:[
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+    ]
+},
+{
+    playListID:6,
+    playlist_name: "Happy", 
+    playlist_author: "me",
+    playlist_art:"assets/img/playlist.png",
+    songs:[
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+    ]
+},
+{
+    playListID:7,
+    playlist_name: "Happy", 
+    playlist_author: "me",
+    playlist_art:"assets/img/playlist.png",
+    songs:[
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+    ]
+},
+{
+    playListID:8,
+    playlist_name: "Happy", 
+    playlist_author: "me",
+    playlist_art:"assets/img/playlist.png",
+    songs:[
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+        {
+            title:"watch",
+            artist:"billie eilish",
+            album:"don't smile at me",
+        },
+    ]
+}
+];
+
+// const create_cards=(playlists)=>{
+//     const container=document.querySelector('.playlist-cards')
+//     container.innerHTML=""
+//     playlists.forEach(playlist => {
+//         const card=document.createElement('div')
+//         card.classList.add('playlist-card')
+//         card.dataset.id=playlist.plylistID
+//         card.innerHTML=`
+//             <img src="${playlist.art}">
+//             <h2 class="playlist-title">${playlist.playlist_name}</h2>
+//             <p class="playlist-author">${playlist.playlist_author}</h2>
+            
+//         `
+//     container.appendChild(card)
+        
+//     });
+
+// }
